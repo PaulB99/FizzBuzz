eae513e7fb8db1b003ffd64db10e6a9882f97ac6
PaulB99 <51514256+PaulB99@users.noreply.github.com>
2020-03-27 19:22:12 +0000

    java fizzbuzz

---
+++ fizzBuzz.java
@@ -0,0 +1,23 @@
+
+public class fizzBuzz {
+	
+	public static void main(String[] args) {
+		for (int i=1; i < 101; i++) {
+			System.out.println(evaluate(i));
+		}
+	}
+	public static String evaluate(int i) {
+		if (i % 5 == 0 && i % 3 == 0) {
+			return "FizzBuzz";
+		}
+		else if (i % 5 == 0) {
+			return "Buzz";
+		}
+		else if (i % 3 == 0) {
+			return "Fizz";
+		}
+		else {
+			return String.valueOf(i);
+		}
+	}
+}
