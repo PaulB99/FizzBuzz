c751d65a50c84a30b0a2d1ac81406254889eae03
PaulB99 <51514256+PaulB99@users.noreply.github.com>
2020-03-27 23:00:16 +0000

    Haskell fizzBuzz

---
+++ fizzBuzz.hs
@@ -0,0 +1,9 @@
+-- Haskell implementation of FizzBuzz
+
+solve :: Int -> String
+solve x = if rem x 3 == 0 && rem x 5 == 0 then "FizzBuzz"
+	  else if rem x 3 == 0 then"Fizz"
+	  else if rem x 5 == 0 then "Buzz"
+	  else show x
+
+run = [solve x | x <- [1..100]]
