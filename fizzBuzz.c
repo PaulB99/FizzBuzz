ec101b21060be0f38e2550d84dcf66fdeca03f8c
PaulB99 <51514256+PaulB99@users.noreply.github.com>
2020-05-03 20:28:03 +0100

    C and C++ implementations

---
+++ fizzBuzz.c
@@ -0,0 +1,35 @@
+// fizzBuzz implementation in C
+
+#include <stdio.h>
+typedef enum {false, true} bool;
+
+bool fizz(int i) {
+	if(i % 3 == 0) { 
+		return true; 
+	}else return false;
+}
+
+bool buzz(int i) {
+	if(i % 5 == 0) {
+		return true; 
+	}else return false;
+}
+
+int main(void) {
+	for (int i=1; i<101; i++) {
+		if (fizz(i) && buzz(i)) {
+			printf("FizzBuzz \n"); 
+		}
+		else if (fizz(i)) {
+			printf("Fizz \n");
+		}
+		else if(buzz(i)) {
+			printf("Buzz \n");
+		}
+		else {
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
+
